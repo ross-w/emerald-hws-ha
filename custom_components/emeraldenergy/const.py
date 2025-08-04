d559253363cc1f4bdf972ed6b602ac25be6f1657
Ross Williamson <ross@inertia.net.nz>
2025-08-04 11:44:08 +1000

    feat: configurable timeouts

--- custom_components/emeraldenergy/const.py
+++ custom_components/emeraldenergy/const.py
@@ -1,3 +1,11 @@
 """Constants for the Emerald Hot Water System integration."""
 
 DOMAIN = "emeraldenergy"
+
+# Configuration constants
+CONF_CONNECTION_TIMEOUT = "connection_timeout"
+CONF_HEALTH_CHECK = "health_check"
+
+# Default values
+DEFAULT_CONNECTION_TIMEOUT = 720  # 12 hours in minutes
+DEFAULT_HEALTH_CHECK = 60  # 1 hour in minutes
