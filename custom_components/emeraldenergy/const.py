c2dc1f3446082350716f297465afbe95f174930b
Ross Williamson <ross@inertia.net.nz>
2025-10-14 17:05:32 +1100

    fix: const

--- custom_components/emeraldenergy/const.py
+++ custom_components/emeraldenergy/const.py
@@ -3,6 +3,8 @@
 DOMAIN = "emeraldenergy"
 
 # Configuration constants
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
 CONF_CONNECTION_TIMEOUT = "connection_timeout"
 CONF_HEALTH_CHECK = "health_check"
 CONF_ENABLE_ENERGY_MONITORING = "enable_energy_monitoring"
