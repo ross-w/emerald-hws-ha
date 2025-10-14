fe63f4568f83a89913eaa6329a4998cb3c7971b5
Ross Williamson <ross@inertia.net.nz>
2025-10-14 16:30:57 +1100

    feat: energy monitoring

--- custom_components/emeraldenergy/const.py
+++ custom_components/emeraldenergy/const.py
@@ -5,7 +5,9 @@ DOMAIN = "emeraldenergy"
 # Configuration constants
 CONF_CONNECTION_TIMEOUT = "connection_timeout"
 CONF_HEALTH_CHECK = "health_check"
+CONF_ENABLE_ENERGY_MONITORING = "enable_energy_monitoring"
 
 # Default values
 DEFAULT_CONNECTION_TIMEOUT = 720  # 12 hours in minutes
 DEFAULT_HEALTH_CHECK = 60  # 1 hour in minutes
+DEFAULT_ENABLE_ENERGY_MONITORING = True
